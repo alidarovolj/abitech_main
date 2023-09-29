ca6b6d8df25d8eac85592964d05c382f042cd16f
alidarovolj <oljasalidarov@gmail.com>
2023-09-29 23:41:43 +0600

    initial commit

--- src/views/MainPage/index.vue
+++ src/views/MainPage/index.vue
@@ -1,25 +1,10 @@
 <template>
-  <div>
-    <HeroComponent/>
-<!--    <About/>-->
-    <Catalog/>
+  <div class="container mx-auto px-4 lg:px-0">
   </div>
 </template>
 
 <script>
-import HeroComponent from "@/views/MainPage/components/Hero.vue";
-import Catalog from "@/views/MainPage/components/Catalog.vue";
-// import About from "@/views/MainPage/components/About.vue";
-import {useMeta} from "vue-meta";
-
 export default {
   name: "MainPage",
-  setup() {
-    useMeta({title: "Главная"});
-  },
-  components: {
-    // About,
-    Catalog,
-    HeroComponent},
-};
+}
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,11 @@ export default {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], darkMode: "class", theme: {
         extend: {
             colors: {
-                mainColor: "#D62D2D",
-                blackColor: "#373737",
-                whiteColor: "#f3f3f4",
-                darkBg: "#131313",
-                darkBgColor: "#373737"
+                mainColor: "#1E3D37",
+                secondaryColor: "#ECB454"
             }, width: {
                 half: "49%", third: "32%", sixth: "16%", fourth: "24.5%", fifth: "19%"
-            },
+            }
         },
     }, plugins: [],
 };
