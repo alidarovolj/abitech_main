dc50f8f9ac209895a2045266ae58f4968c4d3bc0
alidarovolj <oljasalidarov@gmail.com>
2023-10-04 02:21:06 +0600

    fixes

--- src/views/MainPage/components/Insights.vue
+++ src/views/MainPage/components/Insights.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="h-screen py-24 flex items-center justify-center relative w-full">
+  <div class="h-full lg:h-screen py-24 flex items-center justify-center relative w-full">
     <div class="bg-mainColor absolute h-1/2 w-full top-0 -z-10">
     </div>
     <img alt="" class="absolute w-full h-full top-0 left-0 z-10" src="@/assets/img/bg.png">
     <div class="container mx-auto px-4 lg:px-0">
-      <div class="pt-8 pr-8 pb-6 flex items-center">
-        <div v-if="activeBlock === null" class="w-[680px] mr-[52px]">
+      <div class="pt-8 pr-0 lg:pr-8 pb-6 block lg:flex items-center">
+        <div v-if="activeBlock === null" class="w-full lg:w-[680px] mr-0 lg:mr-[52px]">
           <h2 class="text-3xl font-semibold text-secondaryColor mb-14">Insights</h2>
-          <div class="relative cursor-pointer z-30">
+          <div class="relative cursor-pointer z-30 mb-10 lg:mb-0">
             <img alt="" class="rounded-md" src="@/assets/img/insights/kazMunaiGaz.png">
-            <div class="absolute bg-white max-w-[680px] h-1/2 bottom-0 opacity-70 rounded-t-md py-4 px-6">
+            <div class="relative lg:absolute bg-white max-w-[680px] h-1/2 bottom-0 opacity-70 rounded-t-md py-4 px-6">
               <h3 class="text-2xl font-bold">ПРОЕКТ «АСКОУ – ИНВЕНТАРИЗАЦИЯ» В АО «НК «КАЗМУНАЙГАЗ»</h3>
               <p class="text-base">Национальной компанией «КазМунайГаз» в рамках цифровизации в 2020 году была внедрена
                 система автоматизированного учета основных средств «АСКОУ – Инвентаризация».
@@ -17,11 +17,11 @@
             </div>
           </div>
         </div>
-        <div v-else class="w-[680px] mr-[52px] cursor-pointer">
+        <div v-else class="w-full lg:w-[680px] mr-[52px] cursor-pointer">
           <h2 class="text-3xl font-semibold text-secondaryColor mb-14">Insights</h2>
           <div class="relative">
-            <img alt="" class="rounded-md" :src="blocks[activeBlock].img">
-            <div class="absolute bg-white max-w-[680px] h-1/2 bottom-0 opacity-70 rounded-t-md py-4 px-6">
+            <img alt="" class="rounded-t-md rounded-b-none lg:rounded-md" :src="blocks[activeBlock].img">
+            <div class="relative lg:absolute bg-white max-w-[680px] h-1/2 bottom-0 opacity-70 rounded-t-none lg:rounded-t-md rounded-b-md lg:rounded-b-none py-4 px-6">
               <h3 class="text-2xl font-bold">{{ blocks[activeBlock].title }}</h3>
               <p class="text-base">{{ blocks[activeBlock].description }}</p>
             </div>
@@ -29,12 +29,12 @@
         </div>
         <div class="w-full lg:w-[419px]">
           <div v-for="(block, index) of blocks" :key="index" :class="{'!mb-0': blocks.length === index + 1}"
-               class="relative flex z-30 items-center mb-7 bg-white rounded-lg cursor-pointer"
+               class="relative block lg:flex z-30 items-center mb-7 bg-white rounded-lg cursor-pointer"
                @mouseleave="activeBlock = null"
                @mouseover="activeBlock = index">
-            <div class="relative h-[168px] w-[243px]">
+            <div class="relative h-[168px] w-full lg:w-[243px]">
               <div class="absolute left-0 top-0 w-full h-full" style="background: linear-gradient(270deg, #fff -70%, rgba(26, 58, 46, 0.00) 100%);"></div>
-              <img :src="block.img" alt="" class="rounded-l-lg w-[243px] min-w-[243px] h-full object-cover">
+              <img :src="block.img" alt="" class="rounded-l-lg w-full lg:w-[243px] min-w-full lg:min-w-[243px] h-full object-cover">
             </div>
             <p class="text-base font-bold px-6">{{ block.title }}</p>
           </div>
